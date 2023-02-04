e1fdceb4ac884248596a44f1ff23b9b8acf48359
Mohit Sankhla <82494250+Mohit-sankhla02@users.noreply.github.com>
2023-02-04 12:43:16 +0530

    added the functionality of submitting the form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
     const expenses = [
         {
@@ -23,9 +24,13 @@ const App = () => {
         },
       ];
 
+    const addExpenseHandler = (expense) => {
+      console.log('In App.js');
+      console.log(expense);
 
+    };
     return <>
-        <h2>Expenses!</h2>
+        <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses expenses={expenses}/>
     </>
 
