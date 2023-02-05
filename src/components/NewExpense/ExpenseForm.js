09e0ea0300ec116601ec2f381055bf45ecef7ef5
Mohit Sankhla <82494250+Mohit-sankhla02@users.noreply.github.com>
2023-02-05 22:49:59 +0530

    fixed error and added functionality for the year dropdown beneath the newExpense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -71,7 +71,7 @@ const ExpenseForm = (props) => {
                 <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={userInput.enteredAmount}/>
             </div>
             <div className="new-expense__control">
-                <label htmlFor="">Title</label>
+                <label htmlFor="">Date</label>
                 <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={userInput.enteredDate}/>
             </div>
         </div>
